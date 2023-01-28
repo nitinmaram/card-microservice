f892dc0b2b39a8c1693208fdb9b6b63b9eaaaa60
nitinmaram <nitinmaram@gmail.com>
2023-01-28 23:15:23 +0000

    Code refactoring and added nyc for code coverage

--- server/app.js
+++ server/app.js
@@ -1,19 +1,28 @@
-import express, { json } from 'express';
+import express, { json, urlencoded } from 'express';
 import logger from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../swagger.js';
+import { config } from 'dotenv';
 
 import cardRouter from './routes/cardRoutes.js';
+
+config();
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(json());
-app.use('/', cardRouter);
+app.use(urlencoded({ extended: false }));
+app.use(cors());
+app.use(helmet());
 app.use(
   '/api-docs',
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocument)
 );
+
+app.use('/', cardRouter);
 
 export default app;
